3be9aff9952029a268c987c41dca223515b7becd
GP <ganesh.prasannah@oslash.com>
2021-09-15 13:12:32 +0530

    chore(deps): routine upgrade

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
-export { readableStreamFromReader } from "https://deno.land/std@0.106.0/io/mod.ts";
+export * as path from "https://deno.land/std@0.107.0/path/mod.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.107.0/io/mod.ts";
 export { cac } from "https://unpkg.com/cac@6.7.3/mod.ts";
 export { JSZip } from "https://deno.land/x/jszip@0.10.0/mod.ts";
 export { default as debugLog } from "https://deno.land/x/debuglog@v1.0.0/debug.ts";
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,10 +1,10 @@
-export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.107.0/path/mod.ts";
 export {
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.106.0/testing/asserts.ts";
-export { createHash } from "https://deno.land/std@0.106.0/hash/mod.ts";
-export { iter } from "https://deno.land/std@0.106.0/io/mod.ts";
+} from "https://deno.land/std@0.107.0/testing/asserts.ts";
+export { createHash } from "https://deno.land/std@0.107.0/hash/mod.ts";
+export { iter } from "https://deno.land/std@0.107.0/io/mod.ts";
 export { stub } from "https://deno.land/x/mock@v0.10.0/mod.ts";
 export type { Stub } from "https://deno.land/x/mock@v0.10.0/mod.ts";
-export type { SupportedAlgorithm } from "https://deno.land/std@0.106.0/hash/mod.ts";
+export type { SupportedAlgorithm } from "https://deno.land/std@0.107.0/hash/mod.ts";
