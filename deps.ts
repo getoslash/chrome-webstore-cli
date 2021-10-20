da4d0c83323097ce88a3d3c8f9eefc976f0fbe63
GP <ganesh.prasannah@oslash.com>
2021-10-20 12:23:00 +0530

    chore: upgrade dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * as path from "https://deno.land/std@0.107.0/path/mod.ts";
-export { readableStreamFromReader } from "https://deno.land/std@0.107.0/io/mod.ts";
-export { cac } from "https://unpkg.com/cac@6.7.3/mod.ts";
+export * as path from "https://deno.land/std@0.112.0/path/mod.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.112.0/io/mod.ts";
+export { cac } from "https://unpkg.com/cac@6.7.11/mod.ts";
 export { JSZip } from "https://deno.land/x/jszip@0.10.0/mod.ts";
 export { default as debugLog } from "https://deno.land/x/debuglog@v1.0.0/debug.ts";
 export { default as apiClient } from "https://deno.land/x/cwa@v1.0.0/mod.ts";
