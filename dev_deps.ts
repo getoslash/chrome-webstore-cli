c4f48b49ecdda8977233495da33d4b255389bf10
GP <ganesh.prasannah@oslash.com>
2021-12-17 19:33:25 +0530

    chore: upgrades for using Deno v1.17

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,10 +1,10 @@
-export * as path from "https://deno.land/std@0.116.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.118.0/path/mod.ts";
 export {
   assertEquals,
-  assertThrowsAsync,
-} from "https://deno.land/std@0.116.0/testing/asserts.ts";
-export { createHash } from "https://deno.land/std@0.116.0/hash/mod.ts";
-export { iterateReader } from "https://deno.land/std@0.116.0/streams/conversion.ts";
+  assertRejects,
+} from "https://deno.land/std@0.118.0/testing/asserts.ts";
+export { createHash } from "https://deno.land/std@0.118.0/hash/mod.ts";
+export { iterateReader } from "https://deno.land/std@0.118.0/streams/conversion.ts";
 export { resolvesNext, stub } from "https://deno.land/x/mock@0.12.1/mod.ts";
 export type { Stub } from "https://deno.land/x/mock@0.12.1/mod.ts";
-export type { SupportedAlgorithm } from "https://deno.land/std@0.116.0/hash/mod.ts";
+export type { SupportedAlgorithm } from "https://deno.land/std@0.118.0/hash/mod.ts";
